4e78489b645a83db13ee6f493cfe887070a853ce
Ok0iSUV52ZSsLnD <contateste1910101@gmail.com>
2025-07-29 23:21:26 -0300

    Update and rename webhook.js to api/webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.headers.authorization !== "MeuTokenUltraSecreto") {
+  if (req.headers.authorization !== "I0DPYD08D80TT7D97TD7TD7TD07TXT7X97TS97T07T0T7S07TS07S07ST7D0UDUT0UTX08TT08X0UTU0TX07TX07TX08T08T8TC0IGC0IGX80X08X8YX08T08TX8T0X8T0D7TD80TST7PWT8E075W75W70550E57075W570WR70SPTUPTUS8TD0TST7S0UTD0TS0S07SUTPTX"") {
     return res.status(403).json({ error: "Unauthorized" });
   }
 
